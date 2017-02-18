f9b478de5a46009fcfdc2ce2e86e7a6976396f5f
britoigor77 <igorb4696@gmail.com>
2017-02-17 22:57:06 -0200

    Add files via upload

---
+++ TipoInimigo.java
@@ -0,0 +1,20 @@
+package Codigos;
+
+public enum TipoInimigo {
+    FUNKEIRO("O funkeiro puxou o celular e botou pta tocar um proibidão."), 
+    PAGODEIRO("O pagodeiro puxou um cavaquinho e cantou CARACA MULEKE"),
+    EMO("O emo cortou os pulosos e jogou um cd do Restart na sua cara"), 
+    CAPANGA("O capanga te deu um soco");
+    
+    private String nomeHit;
+    
+    TipoInimigo(String nomeHit)
+    {
+        this.nomeHit = nomeHit;
+    }
+    
+    public String getHit()
+    {
+        return this.nomeHit;
+    }
+}
