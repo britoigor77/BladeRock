e21d81673cef993bb0d95f5201879fab4ad0fea3
britoigor77 <igorb4696@gmail.com>
2017-02-17 23:05:27 -0200

    Update TipoFase.java

--- TipoFase.java
+++ TipoFase.java
@@ -1,8 +1,8 @@
 package Codigos;
 
 public enum TipoFase {
-    MUSIC(0),LOSE(0),WON(0), NOTHING(0), BATTLE(2), BATTLE2(3), HEAL(3), HEAL2(5), DAMAGE(3), DAMAGE2(0), TRANSITION(0),
-    GAIN_ITEN(0);
+    MUSIC(0),LOSE(0),WON(0), NOTHING(0), BATTLE(2), BATTLE2(3), HEAL(3), HEAL2(5), DAMAGE(3), DAMAGE2(0), TRANSITION(0)
+    , GANHA_PALHETA(0), GANHA_ACORDAMENTO(0), GANHA_MICROFONE(0);
     
     private final int morePoints;
     
@@ -16,3 +16,4 @@ public enum TipoFase {
         return this.morePoints;
     }
 }
+
